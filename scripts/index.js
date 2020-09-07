4a02d6fae394edaf2a3d32ef36719bcc6c27eb39
Diana Iudina <di-2611@yandex.ru>
2020-09-07 12:04:41 +0300

    комит 3

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,13 @@
 let popup = document.querySelector('.popup');
 let popupOpenButton = document.querySelector('.profile__edit-button');
 let popupCloseButton = document.querySelector('.popup__close');
-let popupSubmit = document.querySelector('.popup__submit');
 
 let formElement = document.querySelector('.popup__content');
 let profileName = document.querySelector('.profile__name');
 let profileProfession = document.querySelector('.profile__profession'); 
 
-let nameInput = document.querySelector('.popup__button_name');
-let jobInput = document.querySelector('.popup__button_profession');
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_profession');
 
 let popupOpen = function () {
 
